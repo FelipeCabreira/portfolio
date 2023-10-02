337c3869966d36f025248102ab6ecff47ca5990b
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-10-01 22:38:53 -0300

    feat: default about me / theme switch

--- components/about.tsx
+++ components/about.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import { useSectionInView } from '@/lib/hooks';
+import { motion } from 'framer-motion';
+import SectionHeading from './section-heading';
+
 export default function About() {
-  return <></>
+  const { ref } = useSectionInView('About');
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about">
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I'm finishing my degree in <span className="font-medium">Information System</span>, but that
+        was not where i found myself engaged with computers, since a young age i was trying to
+        figure it out problems and understand how the{' '}
+        <span className="italic">insides of things works</span> so when i need to choose a career to
+        pursue it was obvious that i should go with one that let me code, build and that challenged
+        myself. to pursue my passion for programming. I enrolled in a coding bootcamp and learned{' '}
+        <span className="font-medium">full-stack web development</span>.{' '}
+        <span className="italic">My favorite part of programming</span> is the problem-solving
+        aspect. I <span className="underline">love</span> the feeling of finally figuring out a
+        solution to a problem. My core stack is{' '}
+        <span className="font-medium">React, Next.js, Node.js, Typescript and MongoDB</span>. I am
+        also familiar with Java/Springboot and SQL. I am always looking to learn new technologies,
+        regarding of the stack. I am currently working for a{' '}
+        <span className="font-medium">ADP Labs</span> as a software developer.
+      </p>
+
+      <p>
+        <span className="font-medium">I am currently learning about</span>.{' '}
+        <span className="font-medium">mobile development</span>, with React Native.
+        <span className="italic">When I'm not coding</span>, I enjoy playing video games, watching
+        movies, animes and reading.
+      </p>
+    </motion.section>
+  );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,8 @@
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
+
 export const links = [
   {
     name: 'Home',
@@ -23,4 +28,92 @@ export const links = [
     name: 'Contact',
     hash: '#contact',
   },
+] as const;
+
+export const experiencesData = [
+  {
+    title: 'DBServer',
+    location: 'Porto Alegre, RS - Brazil',
+    description:
+      'Main activities: Worked as a front-end developer, leading a new team to migrate the legacy project from DELPH to a web strategy using Angular 2.X and .NET, Jasmine, TDD',
+    icon: React.createElement(LuGraduationCap),
+    date: 'May 2018 - Dec 2019',
+  },
+  {
+    title: 'Dell Technologies',
+    location: 'Eldorado do Sul, RS - Brazil',
+    description:
+      'Main activities: Design and architect of a newly core team, using micro-frontend approach with Single-SPA framework/Angular 2.X/Java(Springboot), Jasmine, TDD, SOLID Principles',
+    icon: React.createElement(CgWorkAlt),
+    date: 'Dec 2019 - Dec 2021',
+  },
+  {
+    title: 'Thoughtworks',
+    location: 'Remote',
+    description:
+      'Main activities: Worked as a fullstack developer consultant to ecommerce telecommunication canadian client, using NodeJS/React with Ramda, Jest, TDD',
+    icon: React.createElement(FaReact),
+    date: 'Jan 2022 - Mar 2023',
+  },
+  {
+    title: 'ADP Labs',
+    location: 'Porto Alegre, RS - Brazil',
+    description:
+      'Main activities: Working as a fullstack developer create a new portal to share common knowledge company across all teams, using React with Typescript, Java(Springboot), AWS Cloud Lambdas, TDD, Jest, Solid Principles',
+    icon: React.createElement(FaReact),
+    date: 'April 2022 - current ',
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: 'Linx project',
+    description:
+      'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
+    tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
+    // imageUrl: corpcommentImg,
+  },
+  {
+    title: 'Core data team',
+    description:
+      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
+    // imageUrl: rmtdevImg,
+  },
+  {
+    title: 'TELUS Company',
+    description:
+      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    // imageUrl: wordanalyticsImg,
+  },
+  {
+    title: 'ESI Connect',
+    description:
+      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    // imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'Prisma',
+  'MongoDB',
+  'Redux',
+  'GraphQL',
+  'Apollo',
+  'Express',
+  'PostgreSQL',
+  'Python',
+  'Django',
+  'Framer Motion',
 ] as const;
